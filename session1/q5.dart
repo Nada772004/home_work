df515b878ac0ce6347e54c769149468fa840e4ca
Nada Ahmed <Nada772004>
2025-03-09 00:02:18 +0200

    session1 home work done

---
+++ session1/q5.dart
@@ -0,0 +1,38 @@
+// Question 5
+ //What is the difference between var and dynamic in Dart? Provide an example of
+ //each.
+
+
+ //var
+
+// cannot change type
+// It is a Keyword , It is not a data type
+// Ex
+/*void main() {
+  var name = "menna"; 
+  print(name);
+
+  // name = 25; 
+  // Error: Cannot assign an int to a String variable
+}*/
+
+
+
+ //dynamic
+
+//It contains different types of data, i.e. any data type can be stored in it.
+// It is a weak data type.
+// Ex
+ /*void main() {
+  dynamic value = "nada"; 
+  print(value);
+
+  value = 42; 
+  print(value);
+
+  value = true; 
+  print(value);
+
+  value = 2.4; 
+  print(value);
+}*/
