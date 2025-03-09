692485f7b189dca1bb7c817d52ce0328afbb4df9
Nada Ahmed <Nada772004>
2025-03-09 12:36:42 +0200

    session1 home work done

---
+++ session1/q15.dart
@@ -0,0 +1,11 @@
+ //Question 15
+ //What does it mean for Dart to be a statically-typed language, and how does it
+// impact the way variables are used?
+
+
+
+// The answer
+/*
+This means that when a variable type is assigned, it cannot change to another type.
+Affects all data type except dynamic
+*/
